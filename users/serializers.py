f8dfde2e7e4a771816062df4fc106ed0dbadf8ac
marnixah <marnix@roboco.dev>
2022-11-02 09:56:57 +0100

    fix: :bug: unable to create users due to type

--- users/serializers.py
+++ users/serializers.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 
-def is_valid_type(type):
-    if not type in [User.Types.BUSINESS, User.Types.STUDENT]:
-        raise serializers.ValidationError("Invalid type")
+def is_valid_type(dto):
+    account_type = dto.get("type")
+
+    if account_type == User.Types.APPROVED_BUSINESS:
+        raise serializers.ValidationError(
+            "Cannot create an account with type APPROVED_BUSINESS"
+        )
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
