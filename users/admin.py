f690b8885c83b898518ddbbc2f95a282255d704a
marnixah <marnix@roboco.dev>
2022-11-10 10:43:01 +0100

    feat: :sparkles: add account type to admin panel

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,18 @@ from .models import User
 
 
 class UserAdmin(BaseUserAdmin):
-    pass
+    list_display = [
+        "username",
+        "email",
+        "is_staff",
+        "type",
+    ]
+
+    # Make invites_left editable
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        fieldsets[0][1]["fields"] = ("username", "email", "password", "type")
+        return fieldsets
 
 
 admin.site.register(User, UserAdmin)
